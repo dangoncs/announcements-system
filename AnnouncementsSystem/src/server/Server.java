e726816b4954d1f332cdab13948c58cdef18489f
Danilo Gonçalves <95943821+dangoncs@users.noreply.github.com>
2024-11-21 15:17:54 -0300

    Rename and cleanup project

---
+++ AnnouncementsSystem/src/server/Server.java
@@ -0,0 +1,46 @@
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+import server.gui.ServerGUI;
+
+public class Server extends Thread {
+	private final int port;
+	private ServerSocket serverSocket;
+
+	public Server(int port) {
+		this.serverSocket = null;
+		this.port = port;
+		this.start();
+	}
+	
+	public void run() {
+		try {
+			serverSocket = new ServerSocket(port);
+			System.out.printf("INFO: Escutando na porta %d.\n", port);
+
+			new ServerGUI();
+
+			while(true) {
+				try {
+					new ServerThread(serverSocket.accept());
+				}
+				catch(IOException e) {
+					System.err.println("ERRO: Falha ao conectar com o cliente: " + e.getMessage());
+					break;
+				}
+			}
+		}
+		catch(IOException e) {
+			System.err.printf("ERRO: Falha ao escutar na porta %d: %s\n", port, e.getMessage());
+		}
+		finally {
+			try {
+				serverSocket.close();
+			} catch (IOException e) {
+				System.err.printf("ERRO: Não foi possível fechar a porta %d.\n", port);
+			}
+		}
+	}
+}
---
+++ AnnouncementsSystem/src/server/ServerThread.java
@@ -0,0 +1,68 @@
+package server;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import server.entities.Account;
+import server.services.AccountService;
+import test.AccountTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ServerThread extends Thread {
+	
+	private Socket clientSocket;
+	
+	public ServerThread(Socket clientSocket) {
+		this.clientSocket = clientSocket;
+		this.start();
+	}
+	
+	public void run() {
+		System.out.println("INFO: Conexão estabelecida com novo cliente.");
+	    try {
+	    	PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+	    	BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+	    	
+	    	String inputLine;
+	    	while ((inputLine = in.readLine()) != null) {
+				if (inputLine.equals("0")) break;
+
+				JsonObject jsonObject = JsonParser.parseString(inputLine).getAsJsonObject();
+				System.out.println("Recebido: " + jsonObject);
+
+				String operationCode = jsonObject.get("op").getAsString();
+				switch(operationCode) {
+					case "1":
+						Account accountCreation = new Account();
+						accountCreation.setUser(jsonObject.get("user").getAsString());
+						accountCreation.setPassword(jsonObject.get("password").getAsString());
+						accountCreation.setName(jsonObject.get("name").getAsString());
+
+						new AccountService().create(accountCreation);
+						//TODO: Send appropriate response to client
+						break;
+					case "5":
+						System.out.println("OPERAÇÃO LOGIN");
+						break;
+					case "6":
+						System.out.println("OPERAÇÃO LOGOUT");
+						break;
+					default:
+						System.err.println("OPERAÇÃO NÃO RECONHECIDA");
+				}
+	    	}
+	    	
+	    	out.close();
+	    	in.close();
+	    	clientSocket.close();
+	    	System.out.println("INFO: Conexão fechada por um cliente.");
+	    }
+	    catch (IOException e) {
+	    	System.err.println("ERRO: Problema na comunicação com um cliente: " + e.getMessage());
+	    }
+	}
+}
