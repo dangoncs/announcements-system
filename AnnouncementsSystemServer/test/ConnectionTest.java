7e02a65c40414c025782d314f49a7b232dba8b5c
Danilo Gonçalves <95943821+dangoncs@users.noreply.github.com>
2025-01-21 12:58:35 -0300

    added new tests

---
+++ AnnouncementsSystemServer/test/ConnectionTest.java
@@ -0,0 +1,47 @@
+import main.ServerThread;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+
+public class ConnectionTest {
+
+    public static void main(String[] args) throws IOException {
+        new Thread(ConnectionTest::preconfiguredServer).start();
+        preconfiguredClient();
+    }
+
+    private static void preconfiguredServer() {
+        int port = 24680;
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            Thread thread = new ServerThread(serverSocket.accept());
+            thread.start();
+
+            thread.join();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void preconfiguredClient() throws IOException {
+        String serverHostname = "localhost";
+        int serverPort = 24680;
+
+        List<String> input = JsonTest.scanJsonFromFile("test/json.txt");
+
+        try (Socket socket = new Socket(serverHostname, serverPort);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))
+        ) {
+            for(String line : input) {
+                out.println(line);
+                System.out.println(in.readLine());
+            }
+        }
+    }
+}
--- AnnouncementsSystemServer/test/JsonTest.java
+++ AnnouncementsSystemServer/test/JsonTest.java
@@ -1,20 +1,39 @@
 import main.ServerThread;
+import responses.Response;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonTest {
 
-	public static void main(String[] args) {
-		// Operation example: {"op":"5","user":"a1234567","password":"abc123"}
-		// Response example: {"response":"000", "message":"Successful login", "token":"1234567"}
+	public static void main(String[] args) throws IOException {
+		String input;
 
-		Scanner scanner = new Scanner(System.in);
+		do {
+			System.out.print("Digite o JSON: ");
 
-		System.out.print("Digite o JSON: ");
-		String inputLine = scanner.nextLine();
-		scanner.close();
+			try (BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))) {
+				input = stdIn.readLine();
+			}
 
-		String responseJson = new ServerThread().processJson(inputLine);
-		System.out.printf("Resposta: %s%n", responseJson);
+			Response response = new ServerThread().processJson(input);
+			System.out.printf("Resposta: %s%n", response);
+		} while (input != null && !input.equalsIgnoreCase("exit"));
+	}
+
+	public static List<String> scanJsonFromFile(String filePath) throws IOException {
+		List<String> lines = new ArrayList<>();
+
+		try (BufferedReader in = new BufferedReader(new FileReader(filePath))) {
+			String line;
+			while((line = in.readLine()) != null)
+				lines.add(line);
+		}
+
+		return lines;
 	}
 }
